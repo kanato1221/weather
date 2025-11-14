a9b7abc9718c3483f91aa68a292d15c07ed8a261
kanato1221 <kanatonton2@gmail.com>
2025-11-14 14:26:07 +0900

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by 松井奏人 on 2025/11/14.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
